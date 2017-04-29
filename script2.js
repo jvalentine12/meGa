cde9166f0dba1034f2c7c1ed817782ada76cbe62
jvalentine12 <jvalentine@cristoreyny.org>
2017-04-29 20:30:57 +0000

    readme corrections

--- script2.js
+++ script2.js
@@ -1,7 +1,10 @@
 
 $(document).ready(function(){
     var $_GET = getQueryParams(document.location.search);
-    console.log($_GET)
+    console.log($_GET);
+    $('.loc').each(function(){
+        $(this).html('Location: '+$_GET.state);
+    });
 });
 
 function getQueryParams(qs) {
